cbc230fc917ab5f85966ffc8674a805ab34c498e
renan <renan.rodriguesmoura2022@gmail.com>
2023-02-09 10:58:01 -0300

    pegando tempo da tarefa selecionada

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../common/utils/time";
+import { Itarefas } from "../../types/tarefa";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Stopwhatch.module.scss"
 
-export default function Stopwatch() {
-    console.log('conversão', tempoParaSegundos('01:01:01'));
+interface props {
+    selecionado: Itarefas | undefined
+}
+export default function Stopwatch( {selecionado}: props) {
+    const [tempo, setTempo] = useState<number>()
+
+    useEffect( () => {
+        if(selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado])
+
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
